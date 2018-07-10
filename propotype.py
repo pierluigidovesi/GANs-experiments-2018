a5524d75502b30a426a9a4b4ff169790def3eac8
gianluvillani <35808564+gianluvillani@users.noreply.github.com>
2018-07-10 18:51:55 +0200

    Update propotype.py

--- propotype.py
+++ propotype.py
@@ -328,7 +328,7 @@ print(X_train.shape)
 X_train = np.reshape(X_train, newshape=[-1, OUTPUT_DIM])
 X_test  = np.reshape(X_test, newshape=[-1, OUTPUT_DIM])
 # X_train = np.concatenate((X_train, X_test), axis=0)
-X_train = X_train[:,0,0,0]
+X_train = X_train[:,:,:,0]
 X_train = (X_train - 127.5) / 127.5
 
 # merge and one hot train and test labels
