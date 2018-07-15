54dbc431719ed54cc6bf0614a78a0789ea08a962
pierluigidovesi <pierluigidovesi@gmail.com>
2018-07-15 16:37:10 +0200

    test is

--- our_gan_many_gpu.py
+++ our_gan_many_gpu.py
@@ -29,7 +29,7 @@ cifar10_data = True      # 32 32 (3)
 
 
 # gan architecture
-num_epochs = 50          # tot epochs
+num_epochs = 0          # tot epochs
 batch_size = 64          # micro batch size
 disc_iters = 10          # Number of discriminator updates each generator update. The paper uses 5.
 latent_dim = 128         # input dim (paper 128, but suggested 64)
