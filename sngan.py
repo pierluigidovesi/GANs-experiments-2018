c5b3e7a9201cb69b9ed860500830fbeaa146aaa5
gianluvillani <35808564+gianluvillani@users.noreply.github.com>
2018-08-26 09:13:22 +0200

    Update sngan.py

--- sngan.py
+++ sngan.py
@@ -36,9 +36,9 @@ cifar10_data = True   # 32 32 (3)
 sngan      = True     # spectral normalization
 num_epochs = 300      # tot epochs
 batch_size = 64       # micro batch size
-disc_iters = 10       # Number of discriminator updates each generator update. The paper uses 5.
+disc_iters = 50       # Number of discriminator updates each generator update. The paper uses 5.
 latent_dim = 128      # input dim (paper 128, but suggested 64)
-is_n_batch = 80       # number of batches for EACH class for Inception Score evaluation
+is_n_batch = 10       # number of batches for EACH class for Inception Score evaluation
 
 # Losses parameters
 wasserst_w = 1        # wasserstain weight (always 1)
